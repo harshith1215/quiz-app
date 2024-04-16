93a18a36e30448e0e1d510eb1bd0d1564b2d9beb
Raheema_Shaik <sk.raheema5426@gmail.com>
2024-04-16 21:00:18 +0530

    final gh21 commit

---
+++ app/src/main/java/com/example/myapp_quiz/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myapp_quiz;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "what is 10+26?",
+            "who invented Telephone?",
+            "what is 12*9?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]= {
+            {"32", "42", "36", "38"},
+            {"Graham Bell", "Einstein", "Edison", "None of the above"},
+            {"96", "84", "102", "108"},
+            {"Jeff Bezos", "Elon Musk", "Steve Jobs", "Bill Gates"},
+            {"windows", "Linux", "MacOS", "aLL the above"}
+    };
+    public static String correctAnswers []={
+            "36",
+            "Graham Bell",
+            "108",
+            "Elon Musk",
+            "all of the bove"};
+
+}
+
